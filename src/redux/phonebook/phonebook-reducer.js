01be376a8a58c5cde34a6fa63295a727ba0909eb
Vyacheslav <dikey677@gmail.com>
2022-07-02 17:16:20 +0300

    toolkit update

--- src/redux/phonebook/phonebook-reducer.js
+++ src/redux/phonebook/phonebook-reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
-import types from "./phonebook-types";
+import { createReducer } from "@reduxjs/toolkit";
+import action from "./phonebook-actions";
 
 const CONTACTS = [
   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
@@ -8,30 +9,45 @@ const CONTACTS = [
   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
 ];
 
-const items = (state = CONTACTS, { type, payload }) => {
-  switch (type) {
-    case types.ADD:
-      return [...state, payload];
+// Redux Toolkit
+// Теперь файл types не нужен. Type прописывается первым аргументом в createAction в виде строки.
+// Во время вызова createReducer в теле функции указывается тип action с предварительным импортом файла actions
+// Все форматирование выполняется 'под капотом' Redux Toolkit
+const items = createReducer(CONTACTS, {
+  [action.AddContact]: (state, { payload }) => [...state, payload],
+  [action.DeleteContact]: (state, { payload }) =>
+    state.filter((contact) => contact.id !== payload),
+});
 
-    case types.DELETE:
-      return state.filter((contact) => contact.id !== payload);
-
-    default:
-      return state;
-  }
-};
-
-const filter = (state = "", { type, payload }) => {
-  switch (type) {
-    case types.FILTER:
-      return payload;
-
-    default:
-      return state;
-  }
-};
+const filter = createReducer("", {
+  [action.FilterContact]: (state, { payload }) => payload,
+});
 
 export default combineReducers({
   items,
   filter,
 });
+
+//Redux
+// const items = (state = CONTACTS, { type, payload }) => {
+//   switch (type) {
+//     case types.ADD:
+//       return [...state, payload];
+
+//     case types.DELETE:
+//       return state.filter((contact) => contact.id !== payload);
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const filter = (state = "", { type, payload }) => {
+//   switch (type) {
+//     case types.FILTER:
+//       return payload;
+
+//     default:
+//       return state;
+//   }
+// };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 import phonebookReducer from "./phonebook/phonebook-reducer";
 // Виесто legacy_createStore as createStore, ниже добавляем configureStore
 // import { combineReducers } from "redux"; - включен в configureStore
@@ -15,10 +16,12 @@ import phonebookReducer from "./phonebook/phonebook-reducer";
 
 //Redux Toolkit
 // console.log(process.env.NODE_ENV); - посмотреть, какой режим разработки используется (development/production)
+
 const store = configureStore({
   reducer: {
     contacts: phonebookReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger), // Добавление логгера в midleware (по-умолчанию распыляет весь middleware и добавляет логгер для отслеживания состояний)
   devTools: process.env.NODE_ENV === "development", // Данная строка сообщает - инструменты разрботчика в production доступны не будут --> process+NodeEnvironment+NodeEnvironment
 });
 
